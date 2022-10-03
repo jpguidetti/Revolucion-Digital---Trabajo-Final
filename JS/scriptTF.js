9d9967939bbf6a6787cb028d5b0f54451f81bccb
jpguidetti <juanguitti@gmail.com>
2022-10-03 11:48:25 -0300

    Updated

---
+++ JS/scriptTF.js
@@ -0,0 +1,10 @@
+edad = prompt("Introduce tu edad");
+
+if(edad >= 18){
+    alert("Puedes entrar, eres mayor de edad.");
+} else {
+    alert("Eres menor de edad, entra bajo tu propia responsabilidad.");
+}
+function exit(){
+    alert("Estas seguro que deseas abandonar esta pagina?");
+}
